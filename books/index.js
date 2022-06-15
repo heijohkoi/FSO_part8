a83534f1d71f131f0d69940339d750da8695fa1d
Heikki Koivula <heikki.koivula@gmail.com>
2022-06-15 18:42:59 +0300

    exercise 8.4 complete
    add optional value "author" to allBooks to return all books of the given author, else return all books

--- books/index.js
+++ books/index.js
@@ -109,7 +109,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,7 +119,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author === undefined) {
+        return books
+      }
+      return (
+        books.filter(book => book.author === args.author)
+      )
+    },
     allAuthors: () => {
 
       const byAuthor = _.groupBy(books, (b) => b.author)
